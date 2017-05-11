6595a0dd8631db144268a9a04221d33e5215e9fe
Elina Razina <razina_elina@mail.ru>
2017-05-11 18:01:36 +0300

    2nd practice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound do
+    render json: { error: 'No such record in Database; check params',
+                   status: :not_found,
+    }
+  end
 end
+
+
